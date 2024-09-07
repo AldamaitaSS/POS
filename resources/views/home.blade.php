aceb937388754ff40cd6339238045a522d3e197a
AldamaitaSS <pijarheksa02@gmail.com>
2024-09-07 23:34:51 +0700

    Halaman Home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!-- resources/views/home.blade.php -->
+<html>
+    
+<body>
+    
+    <h1>Selamat Datang di Aplikasi POS</h1>
+    <p>Ini adalah halaman awal website Point of Sales. <br>Pilih kategori produk atau akses halaman penjualan di bawah ini.</p>
+    
+    <nav>
+        <ul>
+            <li><a href="{{ route('category.food-beverage') }}">Makanan & Minuman</a></li>
+            <li><a href="{{ route('category.beauty-health') }}">Kecantikan & Kesehatan</a></li>
+            <li><a href="{{ route('category.home-care') }}">Perawatan Rumah</a></li>
+            <li><a href="{{ route('category.baby-kid') }}">Bayi & Anak</a></li>
+            <li><a href="{{ route('sales') }}">Penjualan</a></li>
+        </ul>
+    </nav>
+
+</body>
+</html>
